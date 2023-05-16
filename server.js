941ab574db5bf807b6a22f0001ce7e0af2223ac6
Paul Jennings <131737981+pjennin11@users.noreply.github.com>
2023-05-15 23:05:55 -0600

    Update server.js
    Aayush help

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let port = process.env.PORT;
 let directory = __dirname + '/public';
 
 /* if we aren't on Heroku, then we need to adjust our port and directory */
-if ((typeof port == 'undefined') || ( port === null)){
+if ((typeof port == 'undefined') || (port === null)){
     port = 8080;
     directory = './public';
 }
@@ -20,8 +20,8 @@ let file = new static.Server(directory);
 
 let app = http.createServer(
     function(request,reponse){
-        request.addListener('end' ,
-            function (){
+        request.addListener('end',
+            function(){
                 file.serve(request,response);
             }
         ).resume();
