f2f870c44bac8ec1a1da688f3102858d09078c5a
pjennin11 <pjennin1@uci.edu>
2023-05-15 23:29:47 -0600

    go

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ let app = http.createServer(
     function(request,reponse){
         request.addListener('end',
             function(){
-                file.serve(request,response);
+                file.serve(request, response);
             }
         ).resume();
     }
